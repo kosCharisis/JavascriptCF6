acd7f49dd6427d40c040ee53458ddf3bff47ed7f
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-16 21:11:15 +0300

    exercises

---
+++ exercises-advanced/unique.js
@@ -0,0 +1,12 @@
+const arr = [1, 2,"a", 2, 3, 3, , , ,"a", "a","g"];
+
+function removeDuplicates(index) {
+    let pure = [];
+    index.forEach(element => {
+        if (!pure.includes(element)) {
+            pure.push(element)
+        }
+    });
+    return pure;
+}
+console.log(removeDuplicates(arr));
