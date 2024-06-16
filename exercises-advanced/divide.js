3a345cf0e36b76e83b7b4782d7c08c8f6a260947
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-16 23:25:21 +0300

    divide

---
+++ exercises-advanced/divide.js
@@ -0,0 +1,14 @@
+const arr = [1, 2,"a", 2, 3, 3, 7, 78, 67,"a", 88, 8, 88,"a","b","a","g"];
+const arr1 = [1, 2,"a","g"];
+
+function divide (index, division) {
+    let arr2 = [];
+    let count = index.length / division.length;
+    for (let i = 0; i < count; i++) {
+        arr2 = arr.splice(0, arr1.length);
+        console.log(arr2);
+    }
+    return count;
+}
+
+console.log(divide(arr, arr1));
