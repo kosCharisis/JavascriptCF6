00554cf37e6fdc440be9694925962aecee8df5ef
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-06-21 20:37:51 +0300

    intro ajax

--- browser/jquery/hello-lorem.js
+++ browser/jquery/hello-lorem.js
@@ -6,4 +6,7 @@ function sayHelloLorem() {
     const $p = $('<p>')
     $p.html(`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, voluptas? Quas accusamus id quasi, exercitationem quibusdam vitae veritatis dicta temporibus, sit ad expedita mollitia accusantium illum voluptatum eos obcaecati dolorem.`)
     $('body').append($p)
+
+    const $body = $(`body`)
+    $body.html(`<p class="">Hello</P>`)
 }
